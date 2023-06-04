1b1abedf31e584faca3ad6e742d13a7d562cd1e7
Harry Jeong <harry@Harrys-MBP.attlocal.net>
2023-06-04 02:28:38 -0700

    react hook page practice

---
+++ pages/forms.tsx
@@ -0,0 +1,62 @@
+import { error } from "console";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onSubmit" });
+  const onValid = (data: LoginForm) => {
+    console.log("im valid");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username  is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+        className={`${
+          Boolean(errors.email?.message) ? "border-red-500 border-1.5" : ""
+        }`}
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", {
+          required: "Password is required",
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="submit" />
+    </form>
+  );
+}
