e1424b31ad25285664e6d8b55a320dfb3b703c36
Harry Jeong <harry@Harrys-MBP.attlocal.net>
2023-06-13 04:47:52 -0700

    update before token

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,72 +1,49 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { Responsetype } from "@libs/server/withHandler";
 import client from "@libs/client/client";
 import { Prisma } from "@prisma/client";
+import twilio from "twilio";
+import { env } from "node:process";
+import { nodeServerAppPaths } from "next/dist/build/webpack/plugins/pages-manifest-plugin";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Responsetype>
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-  });
+  const user = phone ? { phone: phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
   const token = await client.token.create({
     data: {
-      payload: "1234",
+      payload,
       user: {
-        connect: {
-          id: user.id,
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
         },
       },
     },
   });
-
-  console.log(token);
-
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (user) console.log("found it.");
-  //   if (!user) {
-  //     console.log("did not find. will create");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         email,
-  //       },
-  //     });
-  //   }
-  // }
-  // if (phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //   });
-  //   if (user) console.log("found it.");
-  //   if (!user) {
-  //     console.log("did not find. will create");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         phone: +phone,
-  //       },
-  //     });
-  //   }
-  // }
-  console.log(user);
-
-  return res.status(200).end();
+  /*if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.SID,
+      to: process.env.PHONE_NUM!,
+      body: `Your login token is ${payload}.`,
+    });
+    console.log(message);
+  }*/
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
